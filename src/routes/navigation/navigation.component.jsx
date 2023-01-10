abeb916d0af1bbe9fc3cbf2b78c22a25cd9ae9aa
Gurmeet Singh <g.singh91@outlook.com>
2023-01-10 16:55:10 -0600

    got the navigation components working and baslined navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,30 @@
+import {Outlet,Link} from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
+
+const Navigation = () => {
+    return (
+    <Fragment> {/*this is if you dont want to supply a top level tag*/}
+        <div className='navigation'>
+           
+            <Link className='logo-container' to='/'>
+                <CrownLogo className='logo'></CrownLogo>
+            </Link>
+
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+
+        </div>
+        <Outlet />
+    </Fragment>
+    
+     );
+    
+    };
+
+    export default Navigation;

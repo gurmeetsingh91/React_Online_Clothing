e6caaa4ef52f5d7994af6b472f9f325307fa7fdf
Gurmeet <g.singh91@outlook.com>
2023-03-06 15:56:36 -0600

    added the check out feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,10 +3,16 @@ import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
+import { useNavigate } from 'react-router-dom';
 
 
 const CartDropdown = () => {
     const{cartItems} = useContext(CartContext);
+    const navigate =useNavigate();
+
+    const goToCheckoutHandler =() => {
+        navigate('/checkout')
+    };
 
 
     return(
@@ -16,7 +22,7 @@ const CartDropdown = () => {
                     <CartItem key={item.id} cartItem={item} />
                 ))}
             </div>
-            <Button buttonType="inverted">GO to Checkout</Button>
+            <Button onClick={goToCheckoutHandler}>GO to Checkout</Button>
         </div>
 
       
